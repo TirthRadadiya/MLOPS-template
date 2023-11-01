ce9915f9c8e9c1deac36ba3fda467c381af9c631
TirthRadadiya <radadiyatirth@gmail.com>
2023-11-01 18:37:31 +0530

    Data Ingestion stage implemented

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from cnnClassifier import logger
+from cnnClassifier.logging import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Messga from main.py")
+# from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+# from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+# from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
